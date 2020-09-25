d14d7f1f00767798be7c7e2db0799f4dcac08818
phu <phu.tran@logigear.com>
2020-09-25 14:55:55 +0700

    checkin

--- src/data/datatest.ts
+++ src/data/datatest.ts
@@ -4,15 +4,16 @@ export class datatest{
     public static firstname= "logigear";
     public static lastname= "test";
     public static username_nonactive= "logigeartest@logigear.com" ;    
-    public static password_nonactive = "123456";
+    public static password_nonactive = "1";
     public static titlename = "logigeartest";
     public static company ="logigear";
     public static country ="Vietnam";
     public static state = "Ho Chi Minh";
     public static phone = "090999999";
     //page thank you
-    public static title= "Thank you!";
+    public static textheader= "Thank you!";
+    public static user = "logigear test";
     public static textEmail = "Please check your email";
-    public static detail = "and click the link to activate your account. Then download Gondola and follow the steps below to get started."
+   
     
 }
